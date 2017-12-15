555c1748e2f5f2011f5701c6b663eddd0d9d1223
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-12-15 13:06:11 -0500

    hyper + tokio

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,25 @@
 extern crate futures;
-extern crate native_tls;
+extern crate hyper;
 extern crate tokio_core;
-extern crate tokio_io;
-extern crate tokio_tls;
 
-use std::io;
-use std::net::ToSocketAddrs;
-
-use futures::Future;
-use native_tls::TlsConnector;
-use tokio_core::net::TcpStream;
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use hyper::Client;
 use tokio_core::reactor::Core;
-use tokio_tls::TlsConnectorExt;
 
 fn main() {
     let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let addr = "www.rust-lang.org:443".to_socket_addrs().unwrap().next().unwrap();
+    let client = Client::new(&core.handle());
 
-    let cx = TlsConnector::builder().unwrap().build().unwrap();
-    let socket = TcpStream::connect(&addr, &handle);
+    let uri = "http://httpbin.org/ip".parse();
+    let work = client.get(uri.unwrap()).and_then(|res| {
+        println!("Response: {}", res.status());
 
-    let tls_handshake = socket.and_then(|socket| {
-        let tls = cx.connect_async("www.rust-lang.org", socket);
-        tls.map_err(|e| {
-            io::Error::new(io::ErrorKind::Other, e)
+        res.body().for_each(|chunk| {
+            io::stdout()
+                .write_all(&chunk)
+                .map_err(From::from)
         })
     });
-    let request = tls_handshake.and_then(|socket| {
-        tokio_io::io::write_all(socket, "\
-            GET / HTTP/1.0\r\n\
-            Host: www.rust-lang.org\r\n\
-            \r\n\
-        ".as_bytes())
-    });
-    let response = request.and_then(|(socket, _request)| {
-        tokio_io::io::read_to_end(socket, Vec::new())
-    });
-
-    let (_socket, data) = core.run(response).unwrap();
-    println!("{}", String::from_utf8_lossy(&data));
+    core.run(work).unwrap();
 }
