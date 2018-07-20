54ebf9460e56c29783532cbb5cb9a7c0aba46465
Taylor Jones <taylorjonessoftware@gmail.com>
2018-07-20 14:43:01 -0400

    comments

--- src/main.rs
+++ src/main.rs
@@ -25,16 +25,26 @@ fn main() {
     let mailchimp_api_key = env::var("MAILCHIMP_API_KEY").expect("Mailchimp API Key not found in config");
     let mailchimp_username = env::var("MAILCHIMP_USERNAME").expect("Mailchimp Username not found in config");
 
+    // Creating and configuring out HTTPS connector.
     let https = HttpsConnector::new(4).expect("TLS initialization failed");
     let client = Client::builder()
         .build::<_, hyper::Body>(https);
+
+    // Creating the request and setting its attributes
     let mut request = Request::default();
+    // Casting our ENV mailchimp url as a Uri object
     let uri: Uri = mailchimp_url.parse().unwrap();
 
+    // Merging the username and api key from our ENV together like:
+    //  username:api_key
+    // 
+    // We then encode this combination string via base64 so that we can 
+    // insert it into our Authorization header.
     let credential_string = [mailchimp_username, mailchimp_api_key].join(":");
     let encoded_credentials = encode(&credential_string);
     let full_credentials = format!("Basic {}", encoded_credentials);
 
+    // Since our request is a mutex, we set our attributes like this.
     *request.method_mut() = Method::GET;
     *request.uri_mut() = uri.clone();
     request.headers_mut().insert("content-type", HeaderValue::from_str("application/json").unwrap());
@@ -49,7 +59,9 @@ fn main() {
         println!("Headers: {:#?}", res.headers());
 
         res.into_body()
-            // Body is a stream, so as each chunk arrives...
+            // Our response body comes in the form of a stream.
+            // Because of this, we want to print out each part as it 
+            // comes into view. 
             .for_each(|chunk| {
                 io::stdout()
                     .write_all(&chunk)
@@ -64,6 +76,8 @@ fn main() {
     .map_err(|err| {
         println!("Error: {}", err);
     });
+
+    // The request is executed here via rt::run. 
     rt::run(post);
 }
 
